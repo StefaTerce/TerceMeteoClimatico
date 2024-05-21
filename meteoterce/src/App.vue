9e764893507eb31c92c0dd8a1f4d6db7343de303
Stefano Terceros <tercerosstefano@gmail.com>
2024-05-21 09:17:44 +0000

    Setting Graphic

--- meteoterce/src/App.vue
+++ meteoterce/src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer" color="blue">
+      <v-list>
+        <v-list-item v-for="(item, i) in items" :key="i" link :to="item.route" @click="closeDrawer">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="blue" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Terce Meteo Dati</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { text: 'Grafico', icon: 'mdi-chart-line', route: '/grafico' },
+        { text: 'Tabella', icon: 'mdi-table', route: '/tabella' },
+      ]
+    }
+  },
+  methods: {
+    closeDrawer() {
+      this.drawer = false;
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+/* Add your custom styles here */
 </style>
--- meteoterce/src/main.js
+++ meteoterce/src/main.js
@@ -4,11 +4,12 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Importa le icone
 
 const vuetify = createVuetify({
-    components,
-    directives,
-  })
+  components,
+  directives,
+})
 
 import App from './App.vue'
 import router from './router'
